3fc020a1a3ed59ec477440bfeb820750764e8af4
alekain <134544150+alekain@users.noreply.github.com>
2023-09-18 08:36:18 -0400

    Delete Config/database.js

--- Config/database.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const connectionString = "mongodb://localhost:27017/my-database";
-
-mongoose.connect(connectionString, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
